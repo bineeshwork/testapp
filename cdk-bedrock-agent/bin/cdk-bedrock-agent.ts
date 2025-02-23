95f0893ec6cea4e29a2f1a9bb540fd597324a8e4
Bineesh Ravindran <57544485+bineeshwork@users.noreply.github.com>
2025-02-23 15:19:27 -0600

    Update cdk-bedrock-agent.ts

--- cdk-bedrock-agent/bin/cdk-bedrock-agent.ts
+++ cdk-bedrock-agent/bin/cdk-bedrock-agent.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkBedrockAgentStack } from '../lib/cdk-bedrock-agent-stack';
 
 const app = new cdk.App();
-new CdkBedrockAgentStack(app, 'CdkBedrockAgentStack', {
+new CdkBedrockAgentStack(app, 'StudentFeedbackStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
